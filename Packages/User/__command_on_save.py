ed8790df99ad0f74911f7276c76873ac092cf45d
inu1255 <929909260@qq.com>
2017-03-02 13:43:47 +0800

    增加cmd+.,cmd+k配置keymap

--- Packages/User/__command_on_save.py
+++ Packages/User/__command_on_save.py
@@ -15,6 +15,7 @@ def goInstall(iDir):
     except:
         return
 
+# 保存时运行 go install 
 class SaveAndInstall(sublime_plugin.EventListener):
     def on_post_save_(self, view):
         name = view.file_name()
@@ -37,6 +38,7 @@ class SaveAndInstall(sublime_plugin.EventListener):
 #         view.run_command("gs_doc",{"mode":"goto"})
 #         view.window().run_command("godef")
 
+# 运行 go install
 class GoGoInstallCommand(sublime_plugin.TextCommand):
     def run(self,edit):
         view = self.view
@@ -51,7 +53,7 @@ class GoGoInstallCommand(sublime_plugin.TextCommand):
 
 BAIDU_AC = "https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd=%s&json=1&p=3&sid=1431_19034_21121_17001_20718&req=2&csor=2&cb=jQuery110208435955665111006_1483665242750&_=1483665242754"
 import urllib,json
-
+# 百度找词,.md 时使用
 class BaiduAutocomplete(sublime_plugin.EventListener):
     def on_query_completions(self, view, prefix, locations):
         name = view.file_name()
@@ -66,6 +68,7 @@ class BaiduAutocomplete(sublime_plugin.EventListener):
         sugs = [ [prefix+"("+s+")",s] for s in p["s"]]
         return sugs
 
+# 执行选中部分，并将输出按行替换
 class SelectExecCommand(sublime_plugin.TextCommand):
     def run(self,edit):
         view = self.view
@@ -88,3 +91,9 @@ class SelectExecCommand(sublime_plugin.TextCommand):
             output = os.popen(command).read()
             output = output.replace("\n","$1"+s)[:-len(s)]
             view.run_command("insert_snippet",args={"contents": output})
+
+class TestInuCommand(sublime_plugin.TextCommand):
+    def run(self,edit):
+        name = view.file_name()
+        if (not name.lower().endswith('.go')):
+            return
